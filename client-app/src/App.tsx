53d8d28ec1ba661f1835a2021c27c0fdf7f5e295
Arda Kurt <to.arda.kurt@gmail.com>
2023-03-15 09:50:23 +0000

    Git Fix

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Button, Header, List } from 'semantic-ui-react';
 
 function App() {
 
@@ -10,22 +11,22 @@ function App() {
   useEffect(()=> {
     axios.get("http://localhost:5000/api/activities")
     .then(response => {
-      console.log(response)
       setActivities(response.data);
     })
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+        <Header as="h2" icon='users' content="Reactivities"/>
         
-        <ul>
+        <List>
+
         {activities.map((activity: any) => (
-          <li key={activity.id}>{activity.title}</li>
+          <List.Item key={activity.id}>{activity.title}</List.Item>
         ))}
-        </ul>
+
+        </List>
         
-      </header>
     </div>
   );
 }
